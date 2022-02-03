ea7b426b92944485b256af4f5a32b6dc6b8309da
Chithiraipavai_MN <chithiraipavaimn@gmail.com>
2022-02-03 14:07:58 +0530

    Second Commit

--- RightAngle.java
+++ RightAngle.java
@@ -5,7 +5,7 @@ public class RightAngle
     public static void main(String[] args)
     {
        Scanner in=new Scanner(System.in);
-        System.out.println("Enter the N: ");
+        System.out.println("Enter  N: ");
         int N=in.nextInt();
         int row,col;
         for(row=1;row<=N;row++)
